f348cf2007e8c935a8d44f9477c1a53febef6469
misfo <tedwardo2@gmail.com>
2012-02-01 23:52:53 -0600

    Move OS-specific stuff into settings

--- shell_turtlestein.py
+++ shell_turtlestein.py
@@ -1,4 +1,4 @@
-import os, re, subprocess
+import re, subprocess
 import sublime, sublime_plugin
 from functools import partial
 
@@ -10,11 +10,13 @@ def cwd_for_window(window):
 		sublime.status_message("There must be one folder open in the window")
 		return None
 
+def settings():
+	return sublime.load_settings('Shell Turtlestein.sublime-settings')
+
 def exec_config(cmd):
-	settings = sublime.load_settings('Shell Turtlestein.sublime-settings')
 	try:
 		return (c for c
-			    in settings.get('exec_configs')
+			    in settings().get('exec_configs')
 			    if re.search(c.pop('cmd_regex'), cmd)).next()
 	except StopIteration:
 		return None
@@ -36,19 +38,11 @@ class ShellInputCommand(sublime_plugin.WindowCommand):
 							  shell=True)
 			view = self.window.show_input_panel(cwd + " $", "",
 												on_done, None, None)
-			if os.name == "nt":
-				syntax = "Packages/Batch File/Batch File.tmLanguage"
-			else:
-				syntax = "Packages/ShellScript/Shell-Unix-Generic.tmLanguage"
-			view.set_syntax_file(syntax)
+			view.set_syntax_file(settings().get('input_syntax_file'))
 			view.settings().set('gutter', False)
 
 class LaunchShellCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		cwd = cwd_for_window(self.window)
 		if cwd:
-			if os.name == "nt":
-				cmd = ["cmd.exe"]
-			else:
-				cmd = ["Terminal.app"]
-			subprocess.Popen(cmd, cwd=cwd)
+			subprocess.Popen(settings().get('terminal_cmd'), cwd=cwd)
