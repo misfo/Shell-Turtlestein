b97710d80425eb20bcbcc98282315e2541622e9f
misfo <tedwardo2@gmail.com>
2012-02-02 17:33:41 -0600

    Add some documentation, remove non-core cmd_config

--- shell_turtlestein.py
+++ shell_turtlestein.py
@@ -3,6 +3,16 @@ import sublime, sublime_plugin
 from functools import partial
 
 def cwd_for_window(window):
+    """
+    Return the working directory in which the window's commands should run.
+
+    In the common case when the user has one folder open, return that.
+    Otherwise, return one of the following (in order of preference):
+        1) One of the open folders, preferring a folder containing the active
+           file.
+        2) The directory containing the active file.
+        3) The user's home directory.
+    """
     folders = window.folders()
     if len(folders) == 1:
         return folders[0]
